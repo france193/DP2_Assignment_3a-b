d3e69e8cd904e0baeb920a2b5292f3624b6a3a84
Francesco Longo <france193.htc@gmail.com>
2017-01-25 18:13:31 +0100

    - new zip

--- src/it/polito/dp2/NFFG/sol3/client1/models/NffgService/Localhost_NffgServiceRest.java
+++ src/it/polito/dp2/NFFG/sol3/client1/models/NffgService/Localhost_NffgServiceRest.java
@@ -18,7 +18,7 @@ import com.sun.jersey.api.uri.UriTemplate;
 
 @Generated(value = {
     "wadl|http://localhost:8080/NffgService/rest/application.wadl"
-}, comments = "wadl2java, http://wadl.java.net", date = "2017-01-25T18:03:23.731+01:00")
+}, comments = "wadl2java, http://wadl.java.net", date = "2017-01-25T18:11:52.600+01:00")
 public class Localhost_NffgServiceRest {
 
     /**
